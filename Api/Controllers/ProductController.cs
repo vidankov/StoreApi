36fc01dcfaccc08e45d94fe93a358dcd9b5e0e95
vidankov <slavavinsent@gmail.com>
2025-05-29 13:57:46 +0300

    Создан базовый контроллер StoreController; Создан ProductController; Исправлена ошибка генерации Swagger

---
+++ Api/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class ProductController : StoreController
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return Ok(await Task.FromResult<string>("Hello world!"));
+        }
+    }
+}
---
+++ Api/Controllers/StoreController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+    }
+}
