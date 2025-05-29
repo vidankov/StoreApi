767b3752d15e9c3b089604296f38e7e3bb21862c
vidankov <slavavinsent@gmail.com>
2025-05-29 13:59:59 +0300

    Добавлена строчка builder.Services.AddEndpointsApiExplorer();

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
