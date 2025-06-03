09a56ef01ad74fe0b2aa389372b142cdde9ebde7
vidankov <slavavinsent@gmail.com>
2025-06-03 13:52:51 +0300

    Добавлен класс контекста БД, подключен в Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,6 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -6,6 +9,12 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL"));
+});
+
 
 var app = builder.Build();
 
