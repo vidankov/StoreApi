cd0778ce2d10a82e6b5d1ae57d87be5d99b1e3e2
vidankov <slavavinsent@gmail.com>
2025-06-06 14:54:51 +0300

    Реализован эндпойнт для получения продукта по Id

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Api.Data;
+using Api.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace Api.Controllers
 {
@@ -11,8 +13,42 @@ namespace Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetProducts()
         {
-            return Ok(await dbContext.Products.ToListAsync());
+            return Ok(new ResponseServer
+            {
+                StatusCode = HttpStatusCode.OK,
+                Result = await dbContext.Products.ToListAsync()
+            });
         }
-        
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var response = new ResponseServer();
+
+            if (id <= 0)
+            {
+                response.StatusCode = HttpStatusCode.BadRequest;
+                response.IsSuccess = false;
+                response.ErrorMessages.Add("Id должен быть больше нуля.");
+
+                return BadRequest(response);
+            }
+
+            var product = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+            {
+                response.StatusCode = HttpStatusCode.NotFound;
+                response.IsSuccess = false;
+                response.ErrorMessages.Add("Продукт с указанным id не найден.");
+
+                return BadRequest(response);
+            }
+
+            response.StatusCode = HttpStatusCode.OK;
+            response.Result = product;
+
+            return Ok(response);
+        }
     }
 }
---
+++ Api/Model/ResponseServer.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Api.Model
+{
+    public class ResponseServer
+    {
+        public ResponseServer()
+        {
+            IsSuccess = true;
+            ErrorMessages = [];
+        }
+        public bool IsSuccess { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
