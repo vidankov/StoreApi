0919f8f38c0a3166f0f2e3eede6e891669a9df5f
vidankov <slavavinsent@gmail.com>
2025-06-11 15:22:53 +0300

    Реализован эндпойнт для обновления данных продукта, а также модель DTO для этих целей

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Api.ModelDto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Api.Controllers
 {
@@ -104,13 +105,89 @@ namespace Api.Controllers
                     });
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest(new ResponseServer
                 {
                     IsSuccess = false,
                     StatusCode = HttpStatusCode.BadRequest,
-                    ErrorMessages = { "Что-то поломалось", ex.Message}
+                    ErrorMessages = { "Что-то поломалось", ex.Message }
+                });
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ResponseServer>> UpdateProduct(
+                int id, [FromBody] ProductUpdateDto productUpdateDto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (productUpdateDto is null || productUpdateDto.Id != id)
+                    {
+                        return BadRequest(new ResponseServer
+                        {
+                            StatusCode = HttpStatusCode.BadRequest,
+                            IsSuccess = false,
+                            ErrorMessages = { "Несоответствие модели данных" }
+                        });
+                    }
+                    else
+                    {
+                        Product? productFromDb = await dbContext
+                            .Products
+                            .FindAsync(id);
+
+                        if (productFromDb is null)
+                        {
+                            return NotFound(new ResponseServer 
+                            {
+                                IsSuccess = false,
+                                StatusCode = HttpStatusCode.NotFound,
+                                ErrorMessages = { "Продукт с таким id не найден" }
+                            });
+                        }
+
+                        productFromDb.Name = productUpdateDto.Name;
+                        productFromDb.Description = productUpdateDto.Description;
+                        productFromDb.SpecialTag = productUpdateDto.SpecialTag ?? productFromDb.SpecialTag;
+                        productFromDb.Category = productUpdateDto.Category ?? productFromDb.Category;
+                        productFromDb.Price = productUpdateDto.Price;
+
+                        if (productUpdateDto.Image is not null &&
+                            productUpdateDto.Image.Length > 0)
+                        {
+                            productFromDb.Image = $"https://placehold.co/300";
+                        }
+
+                        dbContext.Products.Update(productFromDb);
+                        await dbContext.SaveChangesAsync();
+
+                        return Ok(new ResponseServer 
+                        {
+                            StatusCode = HttpStatusCode.OK,
+                            Result = productFromDb
+                        });
+                    }
+                }
+                else
+                {
+                    return BadRequest(new ResponseServer
+                    {
+                        StatusCode = HttpStatusCode.BadRequest,
+                        IsSuccess = false,
+                        ErrorMessages = { "Модель не соответствует" }
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    IsSuccess = false,
+                    ErrorMessages = { "Что-то пошло не так", ex.Message }
                 });
             }
         }
---
+++ Api/ModelDto/ProductUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.ModelDto
+{
+    public class ProductUpdateDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string? SpecialTag { get; set; }
+        public string? Category { get; set; }
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        public string? Image { get; set; }
+    }
+}
