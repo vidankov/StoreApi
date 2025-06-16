1799133f12576abe1c7612c95ca953a6738a25f0
vidankov <slavavinsent@gmail.com>
2025-06-16 14:40:49 +0300

    Реализована модель DTO для регистрации нового пользователя

---
+++ Api/ModelDto/RegisterRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.ModelDto
+{
+    public class RegisterRequestDto
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+
+    }
+}
