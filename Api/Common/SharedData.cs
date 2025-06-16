14da61c65174401533a5a0611d82ca02d57c71c6
vidankov <slavavinsent@gmail.com>
2025-06-16 13:51:02 +0300

    Добавлен класс SharedData для хранения общих данных, внутри реализован класс Roles для перечисления всех доступных ролей пользователей, реализован инициализатор набора ролей при старте приложения

---
+++ Api/Common/SharedData.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Common
+{
+    public static class SharedData
+    {
+        public static class Roles
+        {
+            public const string Admin = "admin";
+            public const string Consumer = "consumer";
+
+            public static IReadOnlyList<string> AllRoles
+                => new List<string>() { Admin, Consumer };
+        }
+    }
+}
---
+++ Api/Extensions/RollInitializerServiceExtension.cs
@@ -0,0 +1,25 @@
+﻿using Api.Common;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Extensions
+{
+    public static class RollInitializerServiceExtension
+    {
+        public static async Task InitializeRoleAsync(
+            this IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var roleManager = scope
+                .ServiceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+
+            foreach (var role in SharedData.Roles.AllRoles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
