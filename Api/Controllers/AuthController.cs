0acdce7cc2b677c943fa05bf35fd58fc874badd6
vidankov <slavavinsent@gmail.com>
2025-06-16 14:34:51 +0300

    Реализован конструктор для авторизации пользователей

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Api.Data;
+using Api.Model;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Controllers
+{
+    public class AuthController : StoreController
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AuthController(
+            AppDbContext dbContext,
+            UserManager<AppUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+            : base(dbContext)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+    }
+}
