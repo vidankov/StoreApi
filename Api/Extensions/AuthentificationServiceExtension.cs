5d800da8b2c5bfb065738dc78552b4ae14530995
vidankov <slavavinsent@gmail.com>
2025-06-19 11:46:02 +0300

    Реализован метод расширения для настроек конфигурации аутентификации

---
+++ Api/Extensions/AuthentificationServiceExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Api.Extensions
+{
+    public static class AuthentificationServiceExtension
+    {
+        public static IServiceCollection AddAuthentificationConfig(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var authSettingsToken = configuration["AuthSettings:SecretKey"];
+
+            services.AddAuthentication(u =>
+            {
+                u.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                u.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(u => 
+            {
+                u.RequireHttpsMetadata = false;
+                u.SaveToken = true;
+                u.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.ASCII.GetBytes(authSettingsToken)
+                    ),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            return services;
+        }
+    }
+}
