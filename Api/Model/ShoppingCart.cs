91ab17cda5ed91d030b0cb26de767be0117d1bc6
vidankov <slavavinsent@gmail.com>
2025-06-23 14:09:58 +0300

    Добавлена модель данных для корзины товаров

---
+++ Api/Model/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Model
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
+        [NotMapped]
+        public double TotalAmount { get; set; }
+    }
+}
