a09aea382f7958bf724d51176973c1a35be454d3
vidankov <slavavinsent@gmail.com>
2025-06-24 15:37:19 +0300

    Реализован контроллер для работы с корзиной товаров, а также эндпойнт для добавления и/или обновления товаров в корзине, эндпойнт для получения корзины по id пользователя

---
+++ Api/Controllers/ShoppingCartController.cs
@@ -0,0 +1,83 @@
+﻿using Api.Data;
+using Api.Model;
+using Api.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace Api.Controllers
+{
+    public class ShoppingCartController : StoreController
+    {
+        private readonly ShoppingCartService shoppingCartService;
+
+        public ShoppingCartController(AppDbContext dbContext,
+            ShoppingCartService shoppingCartService) : base(dbContext)
+        {
+            this.shoppingCartService = shoppingCartService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseServer>> AppendOrUpdateItemInCart(
+            string userId, int productId, int updateQuantity)
+        {
+            Product? product = await dbContext
+                .Products
+                .FirstOrDefaultAsync(x => x.Id == productId);
+
+            if (product == null)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Товар не найден" }
+                });
+            }
+
+            ShoppingCart? shoppingCart = await dbContext
+                .ShoppingCarts
+                .Include(x => x.CartItems)
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (shoppingCart == null && updateQuantity > 0)
+            {
+                await shoppingCartService.CreateNewCartAsync(userId, productId, updateQuantity);
+            }
+            else if (shoppingCart != null)
+            {
+                await shoppingCartService.UpdateExistingCartAsync(shoppingCart, productId, updateQuantity);
+            }
+
+            return Ok(new ResponseServer
+            {
+                StatusCode = HttpStatusCode.OK
+            });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseServer>> GetShoppingCart(string userId)
+        {
+            try
+            {
+                ShoppingCart shoppingCart = await shoppingCartService
+                    .GetShoppingCartAsync(userId);
+
+                return Ok(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Result = shoppingCart
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Ошибка получения корзины", ex.Message }
+                });
+            }
+        }
+    }
+}
