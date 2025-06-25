406fcde4374980392d8dd71b792d7478199999a3
vidankov <slavavinsent@gmail.com>
2025-06-25 14:31:33 +0300

    Настроен DbSet для OrderDetails и OrderHeaders, выполнена миграция

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -16,6 +16,8 @@ namespace Api.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders{ get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
