4c2a5f4aa6a18d26287e96a8b2ea9653b3e52364
vidankov <slavavinsent@gmail.com>
2025-06-25 13:48:52 +0300

    В SharedData добавлен класс, содержащий возможные статусы заказов

--- Api/Common/SharedData.cs
+++ Api/Common/SharedData.cs
@@ -10,5 +10,15 @@
             public static IReadOnlyList<string> AllRoles
                 => new List<string>() { Admin, Consumer };
         }
+
+        public static class OrderStatus
+        {
+            public const string Pending = "pending";
+            public const string ReadyToShip = "ready_to_ship";
+            public const string Completed = "completed";
+
+            public static IReadOnlyList<string> AllStatus =>
+                new List<string>() { Pending, ReadyToShip, Completed };
+        }
     }
 }
