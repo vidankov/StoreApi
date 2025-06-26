ee05a8e2fcac7cf7b8a0238b5cb76296eb161a2a
vidankov <slavavinsent@gmail.com>
2025-06-26 12:49:14 +0300

    Реализована модель OrderHeaderCreateDto

---
+++ Api/ModelDto/OrderHeaderCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using Api.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.ModelDto
+{
+    public class OrderHeaderCreateDto
+    {
+        [Required]
+        public string CustomerName { get; set; }
+        [Required]
+        public string CustomerEmail { get; set; }
+
+        public string AppUserId { get; set; }
+        public double OrderTotalAmount { get; set; }
+
+        public string Status { get; set; }
+        public int TotalCount { get; set; }
+        public IEnumerable<OrderDetailsCreateDto> OrderDetailsDto { get; set; }
+    }
+}
