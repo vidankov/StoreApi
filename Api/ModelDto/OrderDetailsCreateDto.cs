0a5180e90523b979bf8afde5ae5dab010dfe6d21
vidankov <slavavinsent@gmail.com>
2025-06-26 12:45:56 +0300

    Реализована модель DTO для OrderDetails

---
+++ Api/ModelDto/OrderDetailsCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.ModelDto
+{
+    public class OrderDetailsCreateDto
+    {
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        [Required]
+        public double Price { get; set; }   
+    }
+}
