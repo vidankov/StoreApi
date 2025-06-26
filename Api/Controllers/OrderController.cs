59d8bd0839efd78383ceb9b8978bf49910dead15
vidankov <slavavinsent@gmail.com>
2025-06-26 15:08:28 +0300

    Реализован эндпойнт создания заказа

--- Api/Controllers/OrderController.cs
+++ Api/Controllers/OrderController.cs
@@ -1,5 +1,9 @@
 ﻿using Api.Data;
+using Api.Model;
+using Api.ModelDto;
 using Api.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Api.Controllers
 {
@@ -10,6 +14,42 @@ namespace Api.Controllers
         public OrderController(AppDbContext dbContext, OrdersService ordersService) : base(dbContext)
         {
             this.ordersService = ordersService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseServer>> CreateOrder(
+            [FromBody] OrderHeaderCreateDto orderHeaderCreateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Некорректное состояние модели заказа" }
+                });
+            }
+
+            try
+            {
+                var order = await ordersService.CreateOrderAsync(orderHeaderCreateDto);
+                // order.OrderDetailItems = null;
+
+                return Ok(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.Created,
+                    Result = order
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Ошибка при создании заказа", ex.Message }
+                });
+            }
         }
     }
 }
