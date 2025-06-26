784fb1a1df6234d9537ecb2025a34742034e5459
vidankov <slavavinsent@gmail.com>
2025-06-26 12:40:16 +0300

    Добавлен контроллер для работы с заказами

---
+++ Api/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Api.Data;
+using Api.Service;
+
+namespace Api.Controllers
+{
+    public class ValuesController : StoreController
+    {
+        private readonly OrdersService ordersService;
+
+        public ValuesController(AppDbContext dbContext, OrdersService ordersService) : base(dbContext)
+        {
+            this.ordersService = ordersService;
+        }
+    }
+}
