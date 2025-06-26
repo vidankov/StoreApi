99f40c46efa12433282350b244d28bf76f3fc836
vidankov <slavavinsent@gmail.com>
2025-06-26 12:51:44 +0300

    Реализована модель OrderHeaderUpdateDto

---
+++ Api/ModelDto/OrderHeaderUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using Api.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.ModelDto
+{
+    public class OrderHeaderUpdateDto
+    {
+        public int OrderHeaderId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string Status { get; set; }
+    }
+}
