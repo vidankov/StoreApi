1060613dcb8f4e803f0e94b1d30896522fc7a989
vidankov <slavavinsent@gmail.com>
2025-06-26 13:09:26 +0300

    В OrdersService реализован метод CreateOrderAsync для создания заказов

--- Api/Service/OrdersService.cs
+++ Api/Service/OrdersService.cs
@@ -1,4 +1,7 @@
-﻿using Api.Data;
+﻿using Api.Common;
+using Api.Data;
+using Api.Model;
+using Api.ModelDto;
 
 namespace Api.Service
 {
@@ -9,6 +12,41 @@ namespace Api.Service
         public OrdersService(AppDbContext appDbContext)
         {
             this.appDbContext = appDbContext;
+        }
+
+        public async Task<OrderHeader> CreateOrderAsync(OrderHeaderCreateDto orderHeaderCreateDto)
+        {
+            var order = new OrderHeader
+            {
+                AppUserId = orderHeaderCreateDto.AppUserId,
+                CustomerName = orderHeaderCreateDto.CustomerName,
+                CustomerEmail = orderHeaderCreateDto.CustomerEmail,
+                OrderTotalAmount = orderHeaderCreateDto.OrderTotalAmount,
+                TotalCount = orderHeaderCreateDto.TotalCount,
+                Status = string.IsNullOrEmpty(orderHeaderCreateDto.Status)
+                    ? SharedData.OrderStatus.Pending
+                    : orderHeaderCreateDto.Status
+            };
+
+            await appDbContext.OrderHeaders.AddAsync(order);
+            await appDbContext.SaveChangesAsync();
+
+            foreach (var orderDetailsDto in orderHeaderCreateDto.OrderDetailsDto)
+            {
+                var orderDetails = new OrderDetails
+                {
+                    OrderHeaderId = order.OrderHeaderId,
+                    ProductId = orderDetailsDto.ProductId,
+                    Quantity = orderDetailsDto.Quantity,
+                    ItemName = orderDetailsDto.ItemName,
+                    Price = orderDetailsDto.Price
+                };
+
+                await appDbContext.OrderDetails.AddAsync(orderDetails);
+            }
+
+            await appDbContext.SaveChangesAsync();
+            return order;
         }
     }
 }
