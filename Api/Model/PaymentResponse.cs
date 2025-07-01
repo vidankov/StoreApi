a42f6cbb72759dc06c10786df806b36bae33df96
vidankov <slavavinsent@gmail.com>
2025-07-01 14:18:56 +0300

    Реализована модель PaymentResponse для предоставления ответа по платежу

---
+++ Api/Model/PaymentResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Model
+{
+    public class PaymentResponse
+    {
+        public bool Success { get; set; }
+        public string? IntentId { get; set; }
+        public string? Secret { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
