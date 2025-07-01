54d4fc4e6a1152472756136ba3d011eecc52e3bd
vidankov <slavavinsent@gmail.com>
2025-07-01 14:21:37 +0300

    Реализован интерфейс HandlePaymentAsync

---
+++ Api/Service/Payment/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using Api.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Service.Payment
+{
+    public interface IPaymentService
+    {
+        public Task<ActionResult<ResponseServer>> HandlePaymentAsync(string userId, int orderHeaderId, int cardNumber);
+    }
+}
